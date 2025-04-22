956ad03b101ee9a263fca4a3eb58a9fd308e0dbc
HongdaZ <zyqlxrhd@sina.cn>
2025-04-22 16:43:35 +0800

    torch grad

--- learn.py
+++ learn.py
@@ -1,2 +1,12 @@
 import torch
 torch.cuda.is_available()
+# autograd
+x = torch.arange(4.0)
+x.requires_grad_(True)
+y = 2 * torch.dot(x, x)
+y.backward()
+x.grad
+x.grad.zero_()
+y = x * x
+sum(y).backward()
+x.grad
