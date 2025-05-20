616ec6e0708b0b0875bbe92a924a6cdfdebfbfa7
HongdaZ <zyqlxrhd@sina.cn>
2025-05-20 08:46:07 +0800

    object classification

--- learn.py
+++ learn.py
@@ -318,10 +318,9 @@ train_iter, test_iter = load_data_fashion_mnist(batch_size)
 
 num_inputs = 28 * 28
 num_outputs = 10
-w = torch.normal(0, 0.01, size = (num_inputs, num_outputs),
+W = torch.normal(0, 0.01, size = (num_inputs, num_outputs),
                  requires_grad = True)
 b = torch.zeros(num_outputs, requires_grad = True)
-w = torch.zeros(num_outputs, requires_grad = True)
 def  softmax(X):
     X_exp = torch.exp(X)
     partition = X_exp.sum(1, keepdim = True)
@@ -330,3 +329,112 @@ def  softmax(X):
 X = torch.normal(0, 1, (2, 5))
 X_prob = softmax(X)
 X_prob, X_prob.sum(1)
+
+def net(X):
+    return softmax(torch.matmul(X.reshape((-1, W.shape[0])), W) + b)
+
+y = torch.tensor([0, 2])
+y_hat  = torch.tensor([[.1, .3, .6], [.3, .2, .5]])
+y_hat[[0, 1], y]
+def cross_entropy(y_hat, y):
+    return - torch.log(y_hat[range(len(y_hat)), y])
+
+cross_entropy(y_hat, y)
+
+def accuracy(y_hat, y):
+    if len(y_hat.shape) > 1 and y_hat.shape[1] >1:
+        y_hat = y_hat.argmax(axis = 1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+accuracy(y_hat, y) / len(y)
+
+def evaluate_accuracy(net, data_iter):
+    if isinstance(net, torch.nn.Module):
+        net.eval()
+    metric = Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+class Accumulator:
+    def __init__(self, n):
+        self.data = [0.0] * n
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+    def  __getitem__(self, idx):
+        return self.data[idx]
+evaluate_accuracy(net, test_iter)
+
+def train_epoch_ch3(net, train_iter, loss, updater):
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            l.mean().backward()
+            updater.step()
+        else:
+            l.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+        return metric[0] / metric[2], metric[1] / metric[2]
+
+class Animator:
+    def __init__(self, xlabel = None, ylabel = None, legend = None,
+                 xlim = None, ylim = None, xscale = "linear", yscale = "linear",
+                 fmts = ("-", "m--", "g-.", "r:"), nrows = 1, ncols = 1,
+                 figsize = (3.5, 2.5)):
+        if legend is None:
+            legend = []
+        self.fig, self.axes = plt.subplots(nrows, ncols, figsize = figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes,]
+        plt.gca().set_xlabel(xlabel)
+        plt.gca().set_xlim(xlim)
+        plt.gca().set_ylim(ylim)
+        plt.legend(legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+    def add(self, x, y):
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.Y[i].append(b)
+                self.X[i].append(a)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        display.display(self.fig)
+        display.clear_output(wait = True)
+        plt.show(block = True)
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
+    animator = Animator(xlabel = "epoch", xlim = [1, num_epochs],
+                        ylim = [.3, .9], legend = ["train loss", "train acc", "test acc"])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+    train_loss, train_acc = train_metrics
+    #assert train_loss < .5, train_loss
+    #assert train_acc <= 1 and train_acc > .7, train_acc
+    #assert test_acc <= 1 and test_acc > .7, test_acc
+
+lr = .1
+def updater(batch_size):
+    return sgd([W, b], lr, batch_size)
+
+num_epochs = 10
+train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
