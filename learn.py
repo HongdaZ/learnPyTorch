23b76f33636d26f35d3adb872566093807a95e5e
HongdaZ <zyqlxrhd@sina.cn>
2025-05-20 11:23:00 +0800

    concise code for image classification

--- learn.py
+++ learn.py
@@ -510,3 +510,19 @@ def predict_ch3(net, test_iter, n = 10):
     show_images(X[0 : n].reshape((n, 28, 28)), 1, n, titles = titles[0 : n])
 
 predict_ch3(net, test_iter)
+
+batch_size = 256
+train_iter, test_iter = load_data_fashion_mnist(batch_size)
+net = nn.Sequential(nn.Flatten(),
+                    nn.Linear(784, 10))
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, std = .01)
+net.apply(init_weights)
+
+loss = nn.CrossEntropyLoss(reduction = "none")
+trainer = torch.optim.SGD(net.parameters(), lr = .1)
+
+num_epochs = 10
+train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
