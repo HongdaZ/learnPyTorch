280b01b01334f267d550c8d2ad2939bc71a38025
HongdaZ <zyqlxrhd@sina.cn>
2025-05-22 10:39:35 +0800

    nn with dropout

--- learn.py
+++ learn.py
@@ -498,9 +498,9 @@ def updater(batch_size):
     return sgd([W, b], lr, batch_size)
 
 num_epochs = 10
-train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
+#train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
 
-def predict_ch3(net, test_iter, n = 10):
+def predict_ch3(net, test_iter, n = 6):
     for X, y in test_iter:
         break
     trues = get_fashion_mnist_labels(y)
@@ -525,7 +525,7 @@ loss = nn.CrossEntropyLoss(reduction = "none")
 trainer = torch.optim.SGD(net.parameters(), lr = .1)
 
 num_epochs = 20
-train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
+#train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
 
 x = torch.arange(-8.0, 8.0, 0.1, requires_grad = True)
 y = torch.relu(x)
@@ -581,6 +581,184 @@ loss = nn.CrossEntropyLoss(reduction = "none")
 
 num_epochs, lr = 10, .1
 updater = torch.optim.SGD(params, lr = lr)
-train_ch3(net, train_iter, test_iter, loss, num_epochs, updater)
+#train_ch3(net, train_iter, test_iter, loss, num_epochs, updater)
 
 predict_ch3(net, test_iter)
+
+net = nn.Sequential(nn.Flatten(),
+                    nn.Linear(784, 256),
+                    nn.ReLU(),
+                    nn.Linear(256, 10))
+def init_weights(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, std = .01)
+net.apply(init_weights)
+
+batch_size, lr, num_epochs = 256, .1, 10
+loss = nn.CrossEntropyLoss(reduction = "none")
+trainer = torch.optim.SGD(net.parameters(), lr = lr)
+
+train_iter, test_iter = load_data_fashion_mnist(batch_size)
+#train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
+
+max_degree = 20
+n_train, n_test = 100, 100
+true_w = np.zeros(max_degree)
+true_w[0 : 4] = np.array([5, 1.2, -3.4, 5.6])
+features = np.random.normal(size = (n_train + n_test, 1))
+np.random.shuffle(features)
+poly_features = np.power(features,
+                         np.arange(max_degree).reshape(1, -1))
+for i in range(max_degree):
+    poly_features[:, i] /= math.gamma(i + 1)
+labels = poly_features @ true_w
+labels += np.random.normal(scale = .1, size = labels.shape)
+ture_w, features, poly_features, labels = [
+    torch.tensor(x, dtype = torch.float32) for x in [true_w, features,
+                                                     poly_features, labels]
+]
+
+def evaluate_loss(net, data_iter, loss):
+    metric = Accumulator(2)
+    for X, y in data_iter:
+        out = net(X)
+        y = y.reshape(out.shape)
+        l = loss(out, y)
+        metric.add(l.sum(), l.numel())
+    return metric[0] / metric[1]
+
+def train(train_features, test_features, train_labels, test_labels,
+          num_epochs = 400):
+    loss = nn.MSELoss(reduction = "none")
+    input_shape = train_features.shape[-1]
+    net = nn.Sequential(nn.Linear(input_shape, 1, bias = False))
+    batch_size = min(10, train_labels.shape[0])
+    train_iter = load_array((train_features, train_labels.reshape(-1, 1)),
+                            batch_size)
+    test_iter = load_array((test_features, test_labels.reshape(-1, 1)),
+                           batch_size, is_train = False)
+    trainer = torch.optim.SGD(net.parameters(), lr = .01)
+    animator = Animator(xlabel = "epoch", ylabel = "loss", yscale = "log",
+                        xlim = [1, num_epochs], ylim = [1e-3, 1e2],
+                        legend = ["train", "test"])
+    for epoch in range(num_epochs):
+        train_epoch_ch3(net, train_iter, loss, trainer)
+        if epoch == 0 or (epoch + 1) % 20 == 0:
+            animator.add(epoch + 1, (evaluate_loss(net, train_iter, loss),
+                                     evaluate_loss(net, test_iter, loss)))
+    plt.show(block = True)
+    print("weight:", net[0].weight.data.numpy())
+
+"""train(poly_features[:n_train, :4], poly_features[n_train:, :4],
+      labels[:n_train], labels[n_train:])
+
+train(poly_features[:n_train, :2], poly_features[n_train:, :2],
+      labels[:n_train], labels[n_train:])
+
+train(poly_features[:n_train, :], poly_features[n_train:, :],
+      labels[:n_train], labels[n_train:], num_epochs = 1500)"""
+
+n_train, n_test, num_inputs, batch_size = 20, 100, 200, 5
+true_w, true_b = torch.ones((num_inputs, 1)) * .01, .05
+train_data = synthetic_data(true_w, true_b, n_train)
+train_iter = load_array(train_data, batch_size)
+test_data = synthetic_data(true_w, true_b, n_test)
+test_iter = load_array(test_data, batch_size, is_train = False)
+
+def init_params():
+    w = torch.normal(0, 1, size = (num_inputs, 1), requires_grad = True)
+    b = torch.zeros(1, requires_grad = True)
+    return [w, b]
+
+def l2_penalty(w):
+    return torch.sum(w.pow(2)) / 2
+
+def train(lambd):
+    w, b = init_params()
+    net, loss = lambda X: linreg(X, w, b), squared_loss
+    num_epochs, lr = 100, .003
+    animator = Animator(xlabel = "epochs", ylabel = "loss", yscale = "log",
+                        xlim = [5, num_epochs], legend = ["train", "test"])
+    for epoch in range(num_epochs):
+        for X, y in train_iter:
+            l = loss(net(X), y) + lambd * l2_penalty(w)
+            l.sum().backward()
+            sgd([w, b], lr, batch_size)
+        if (epoch + 1) % 5 == 0:
+            animator.add(epoch + 1, (evaluate_loss(net, train_iter, loss),
+                         evaluate_loss(net, test_iter, loss)))
+    print("w的L2范数是：", torch.norm(w).item())
+
+train(lambd = 0)
+train(lambd = 10)
+
+def train_concise(wd):
+    net = nn.Sequential(nn.Linear(num_inputs, 1))
+    for param in net.parameters():
+        param.data.normal_()
+    loss = nn.MSELoss(reduction = "none")
+    num_epochs, lr = 100, .003
+    trainer = torch.optim.SGD([
+        {"params" : net[0].weight, "weight_decay" : wd},
+        {"params" : net[0].bias}], lr = lr)
+    animator = Animator(xlabel = "epochs", ylabel = "loss", yscale = "log",
+                        xlim = [5, num_epochs], legend = ["train", "test"])
+    for epoch in range(num_epochs):
+        for X, y in train_iter:
+            trainer.zero_grad()
+            l = loss(net(X), y)
+            l.mean().backward()
+            trainer.step()
+        if (epoch + 1) % 5 == 0:
+            animator.add(epoch + 1,
+                         (evaluate_loss(net, train_iter, loss),
+                          evaluate_loss(net, test_iter, loss)))
+    print("w的L2范数：", net[0].weight.norm().item())
+
+train_concise(3)
+
+def dropout_layer(X, dropout):
+    assert 0 <= dropout <= 1
+    if dropout == 1:
+        return torch.zeros_like(X)
+    if dropout == 0:
+        return X
+    mask = (torch.rand(X.shape) > dropout).float()
+    return mask * X / (1.0 - dropout)
+
+X = torch.arange(16, dtype = torch.float32).reshape((2, 8))
+print(X)
+print(dropout_layer(X, 0))
+print(dropout_layer(X, .5))
+print(dropout_layer(X, 1.))
+
+num_inputs, num_outputs, num_hiddens1, num_hiddens2 = 784, 10, 256, 256
+
+dropout1, dropout2 = .2, .5
+class Net(nn.Module):
+    def __init__(self, num_inputs, num_outputs, num_hiddens1, num_hiddens2,
+                 is_training = True):
+        super(Net, self).__init__()
+        self.num_inputs = num_inputs
+        self.training = is_training
+        self.lin1 = nn.Linear(num_inputs, num_hiddens1)
+        self.lin2 = nn.Linear(num_hiddens1, num_hiddens2)
+        self.lin3 = nn.Linear(num_hiddens2, num_outputs)
+        self.relu = nn.ReLU()
+    def forward(self, X):
+        H1 = self.relu(self.lin1(X.reshape((-1, self.num_inputs))))
+        if self.training == True:
+            H1 = dropout_layer(H1, dropout1)
+        H2 = self.relu(self.lin2(H1))
+        if self.training == True:
+            H2 = dropout_layer(H2, dropout2)
+        out = self.lin3(H2)
+        return out
+net = Net(num_inputs, num_outputs, num_hiddens1, num_hiddens2)
+
+num_epochs, lr, batch_size = 10, .5, 256
+loss = nn.CrossEntropyLoss(reduction = "none")
+train_iter, test_iter = load_data_fashion_mnist(batch_size)
+trainer = torch.optim.SGD(net.parameters(), lr = lr)
+
+train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
